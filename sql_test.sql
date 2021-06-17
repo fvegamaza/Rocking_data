ee382f23662c68ff86db9468efba32998baf996f
Fvega <fvegamaza@gmail.com>
2021-06-17 02:06:26 -0300

    twelve finished

--- sql_test.sql
+++ sql_test.sql
@@ -64,3 +64,26 @@ inner join EQUIPOS
 group by EQUIPOS.Nombre
 order by cantidad_jugadores desc
 limit 2
+
+-- Preguntas conceptuales 
+-- 11
+-- Para ocultar el valor le daria este query:
+
+select JUGADORES.nombre,JUGADORES.apellido,EQUIPOS.nombre
+from jugadores
+inner join EQUIPOS
+on JUGADORES.id_equipo = EQUIPOS.id_equipo
+order by valor desc
+
+-- Le diria que esta ordenado de mayor a menor en valor
+
+-- 12
+--Agregaria una columna primary key para identificar cada observacion
+-- Haria lo siguiente segun cada columna:
+-- Nombre y apellido: No deberia haber null, podriamos al actualizar la bases de datos hacer este
+-- campo NOT NULL imposibilitando que vuelva a suceder
+-- Valor: No puede tener valores negativos , o bien reemplazamos el "-" y asumimos
+-- que todo valor negativos es un error humanos, o solo deja insertar valores positivos y 0
+
+
+--13
