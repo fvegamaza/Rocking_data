645db649f96791c7dd74b8149bba1ce381482ebf
Fvega <fvegamaza@gmail.com>
2021-06-17 00:56:49 -0300

    third

--- sql_test.sql
+++ sql_test.sql
@@ -2,6 +2,24 @@
 select JUGADORES.nombre, valor,EQUIPOS.nombre
 from jugadores
 inner join EQUIPOS
+on JUGADORES.id_equipo = EQUIPOS.id_equipo;
+
+--Second
+select valor,EQUIPOS.nombre
+from jugadores
+inner join EQUIPOS
 on JUGADORES.id_equipo = EQUIPOS.id_equipo
+group by EQUIPOS.nombre
+order by valor desc
+limit 1;
 
+--third
 
+select JUGADORES.nombre,JUGADORES.apellido,valor,EQUIPOS.nombre
+from jugadores
+inner join EQUIPOS
+on JUGADORES.id_equipo = EQUIPOS.id_equipo
+order by valor desc
+limit 1
+-- Hay que traer tanto el nombre como el apellido porque si filtras solo por apellido al tener null
+-- no aparece primero el correcto
